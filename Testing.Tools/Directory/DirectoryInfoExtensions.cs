fe1c01632bdfe2477c511c9d3d5cbd53d78ed567
RasheR666 <rasher666@gmail.com>
2019-02-22 14:44:37 +0500

    Directory tools

---
+++ Testing.Tools/Directory/DirectoryInfoExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace Testing.Tools.Directory
+{
+	public static class DirectoryInfoExtensions
+	{
+		public static bool IsRoot(this DirectoryInfo directory)
+		{
+			return Path.GetPathRoot(directory.FullName) == directory.FullName;
+		}
+
+		public static bool Contains(this DirectoryInfo directory, string filename)
+		{
+			return directory.GetFiles().Any(x => x.Name == filename);
+		}
+
+		public static DirectoryInfo GetOrCreateSubDirectory(this DirectoryInfo directory, string directoryName)
+		{
+			var subDirectory = directory.GetDirectories().FirstOrDefault(x => x.Name == directoryName);
+			return subDirectory ?? directory.CreateSubdirectory(directoryName);
+		}
+	}
+}
---
+++ Testing.Tools/Directory/SolutionDirectoryProvider.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Testing.Tools.Directory
+{
+	public static class SolutionDirectoryProvider
+	{
+		public static DirectoryInfo Get(string solutionFilename)
+		{
+			var assemblyDirectory = GetAssemblyDirectory();
+			var currentDirectory = GetCurrentDirectory();
+			var solutionDirectory = FindSolutionDirectory(assemblyDirectory, solutionFilename) ?? FindSolutionDirectory(currentDirectory, solutionFilename);
+
+			if(solutionDirectory == null)
+				throw new Exception($"Не получилось найти директорию с файлом '{solutionFilename}'. Текущая директория '{currentDirectory.FullName}'. Директория со сборкой '{assemblyDirectory.FullName}'");
+
+			return solutionDirectory;
+		}
+
+		private static DirectoryInfo GetAssemblyDirectory()
+		{
+			return new FileInfo(Assembly.GetExecutingAssembly().Location).Directory;
+		}
+
+		private static DirectoryInfo GetCurrentDirectory()
+		{
+			return new DirectoryInfo(System.IO.Directory.GetCurrentDirectory());
+		}
+
+		private static DirectoryInfo FindSolutionDirectory(DirectoryInfo directory, string solutionFilename)
+		{
+			DirectoryInfo parentDirectory;
+			while(!directory.IsRoot())
+			{
+				if(directory.Contains(solutionFilename))
+					return directory;
+
+				parentDirectory = directory.Parent;
+				if(parentDirectory == null)
+					return null;
+				directory = parentDirectory;
+			}
+
+			return null;
+		}
+	}
+}
